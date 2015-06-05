9c84bb437aa8c3f043f46fb1b5225e43e80989dd
satheeshdomainindia <satheesh@dreamstudio.in>
2015-06-05 19:53:59 +0530

    bug fixed
    bug fixed

--- CodeIgniter/public_html/html/default/home/index.php
+++ CodeIgniter/public_html/html/default/home/index.php
@@ -4,7 +4,7 @@
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>{blog_title}</code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
